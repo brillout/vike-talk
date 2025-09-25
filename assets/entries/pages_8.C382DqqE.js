fc77781922f53bc95278ae21ef8fc5339f2a727a
brillout <github.com@brillout.com>
2025-09-25 13:13:31 +0000

    Deploying to gh-pages from @ brillout/vike-talk@4b0cbf971539f3619ac061fcc3ef9e54ab925989 🚀

---
+++ assets/entries/pages_8.C382DqqE.js
@@ -0,0 +1,32 @@
+import{j as e,i as a,a as o,L as l,b as r,c as s,o as d}from"../chunks/chunk-Dt6oqEFt.js";import"../chunks/chunk-C8Q88_sE.js";/* empty css                      *//* empty css                      */import"../chunks/chunk-BPY0c0Pk.js";function n(i){const t={code:"code",h1:"h1",li:"li",ul:"ul",...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.h1,{children:"More..."}),`
+`,e.jsx("div",{style:{marginTop:-30}}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[e.jsx(t.code,{children:"+data.js"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"Data fetching"}),`
+`]}),`
+`]}),`
+`,e.jsxs(t.li,{children:[e.jsx(t.code,{children:"+onCreateGlobalContext.client.js"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"Client-side initialization (e.g. authentication)"}),`
+`,e.jsx(t.li,{children:"Init store (e.g. Redux or Zustand)"}),`
+`]}),`
+`]}),`
+`,e.jsx(t.li,{children:e.jsx(t.code,{children:"+onCreatePageContext.{client,server}.js"})}),`
+`,e.jsxs(t.li,{children:[e.jsx(t.code,{children:"+guard.js"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"Authorization"}),`
+`]}),`
+`]}),`
+`,e.jsxs(t.li,{children:[e.jsx(t.code,{children:"+onPageTransition{Start,End}.js"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"Full control over page transition anmiation"}),`
+`]}),`
+`]}),`
+`,e.jsxs(t.li,{children:[e.jsx(t.code,{children:"+onBeforeRoute.js"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"Full control over i18n integration"}),`
+`]}),`
+`]}),`
+`,e.jsx(t.li,{children:"..."}),`
+`]})]})}function u(i={}){const{wrapper:t}=i.components||{};return t?e.jsx(t,{...i,children:e.jsx(n,{...i})}):n(i)}const p=Object.freeze(Object.defineProperty({__proto__:null,default:u},Symbol.toStringTag,{value:"Module"})),T={serverOnlyHooks:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!1}},isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"vike-react/__internal/integration/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:d}},onPageTransitionStart:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+onPageTransitionStart.ts",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:s}},onPageTransitionEnd:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+onPageTransitionEnd.ts",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:r}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/8/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:p}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"vike-react/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},Layout:{type:"cumulative",definedAtData:[{filePathToShowToUser:"/layouts/LayoutDefault.tsx",fileExportPathToShowToUser:[]}],valueSerialized:[{type:"pointer-import",value:l}]},title:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+config.ts",fileExportPathToShowToUser:["default","title"]},valueSerialized:{type:"js-serialized",value:"Vike Talk"}},onAfterRenderClient:{type:"cumulative",definedAtData:[{filePathToShowToUser:"/pages/+onAfterRenderClient.ts",fileExportPathToShowToUser:[]}],valueSerialized:[{type:"plus-file",exportValues:o}]},Loading:{type:"standard",definedAtData:{filePathToShowToUser:"vike-react/__internal/integration/Loading",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:a}}};export{T as configValuesSerialized};
