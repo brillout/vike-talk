33b6f2e928e97a6b7407bae971ec59bcd88a2842
brillout <github.com@brillout.com>
2025-09-25 13:43:40 +0000

    Deploying to gh-pages from @ brillout/vike-talk@54b5b1d9f2f6ea2decc1e17c38f454df7ce3d9f5 🚀

---
+++ assets/entries/pages_13.BnejMO-L.js
@@ -0,0 +1,25 @@
+import{j as e,i as n,a as o,L as l,b as r,c as s,o as d}from"../chunks/chunk-CX5PfsYO.js";import"../chunks/chunk-C8Q88_sE.js";/* empty css                      *//* empty css                      */import"../chunks/chunk-BPY0c0Pk.js";function i(a){const t={h2:"h2",li:"li",p:"p",ul:"ul",...a.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.h2,{children:"Open Source Pricing"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"100% MIT licensed"}),`
+`,e.jsx(t.li,{children:"As an Engineer, you never need a license — everthing works just like a free open source tool"}),`
+`,e.jsxs(t.li,{children:["Only larger companies need a license",`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"Pay only how much you can/want"}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsx(t.p,{children:"TODO"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:["It isn't a bug, it's a feature",`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"Companies pay a small amount and, in exchange, get a high-quality tool"}),`
+`,e.jsxs(t.li,{children:["No hidden fees — a truly free framework doesn't exist",`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"Next.js is hard to self host"}),`
+`,e.jsx(t.li,{children:"Astro is VC backed => they will have to make money at some point"}),`
+`,e.jsx(t.li,{children:"Frameworks are complex to develop — donations alone cannot cover the cost"}),`
+`]}),`
+`]}),`
+`]}),`
+`]}),`
+`]})]})}function u(a={}){const{wrapper:t}=a.components||{};return t?e.jsx(t,{...a,children:e.jsx(i,{...a})}):i(a)}const p=Object.freeze(Object.defineProperty({__proto__:null,default:u},Symbol.toStringTag,{value:"Module"})),x={serverOnlyHooks:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!1}},isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"vike-react/__internal/integration/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:d}},onPageTransitionStart:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+onPageTransitionStart.ts",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:s}},onPageTransitionEnd:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+onPageTransitionEnd.ts",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:r}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/13/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:p}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"vike-react/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},Layout:{type:"cumulative",definedAtData:[{filePathToShowToUser:"/layouts/LayoutDefault.tsx",fileExportPathToShowToUser:[]}],valueSerialized:[{type:"pointer-import",value:l}]},title:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+config.ts",fileExportPathToShowToUser:["default","title"]},valueSerialized:{type:"js-serialized",value:"Vike Talk"}},onAfterRenderClient:{type:"cumulative",definedAtData:[{filePathToShowToUser:"/pages/+onAfterRenderClient.ts",fileExportPathToShowToUser:[]}],valueSerialized:[{type:"plus-file",exportValues:o}]},Loading:{type:"standard",definedAtData:{filePathToShowToUser:"vike-react/__internal/integration/Loading",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:n}}};export{x as configValuesSerialized};
---
+++ assets/entries/pages_8.Cz4ck_NH.js
@@ -0,0 +1,30 @@
+import{j as t,i as a,a as o,L as l,b as r,c as d,o as s}from"../chunks/chunk-CX5PfsYO.js";import"../chunks/chunk-C8Q88_sE.js";/* empty css                      *//* empty css                      */import"../chunks/chunk-BPY0c0Pk.js";function n(i){const e={code:"code",li:"li",ul:"ul",...i.components};return t.jsxs(e.ul,{children:[`
+`,t.jsxs(e.li,{children:[t.jsx(e.code,{children:"+data.js"}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsx(e.li,{children:"Data fetching"}),`
+`]}),`
+`]}),`
+`,t.jsxs(e.li,{children:[t.jsx(e.code,{children:"+onCreateGlobalContext.client.js"}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsx(e.li,{children:"Client-side initialization (e.g. authentication)"}),`
+`,t.jsx(e.li,{children:"Init store (e.g. Redux or Zustand)"}),`
+`]}),`
+`]}),`
+`,t.jsx(e.li,{children:t.jsx(e.code,{children:"+onCreatePageContext.{client,server}.js"})}),`
+`,t.jsxs(e.li,{children:[t.jsx(e.code,{children:"+guard.js"}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsx(e.li,{children:"Authorization"}),`
+`]}),`
+`]}),`
+`,t.jsxs(e.li,{children:[t.jsx(e.code,{children:"+onPageTransition{Start,End}.js"}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsx(e.li,{children:"Full control over page transition anmiation"}),`
+`]}),`
+`]}),`
+`,t.jsxs(e.li,{children:[t.jsx(e.code,{children:"+onBeforeRoute.js"}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsx(e.li,{children:"Full control over i18n integration"}),`
+`]}),`
+`]}),`
+`,t.jsx(e.li,{children:"..."}),`
+`]})}function u(i={}){const{wrapper:e}=i.components||{};return e?t.jsx(e,{...i,children:t.jsx(n,{...i})}):n(i)}const p=Object.freeze(Object.defineProperty({__proto__:null,default:u},Symbol.toStringTag,{value:"Module"})),T={serverOnlyHooks:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!1}},isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"vike-react/__internal/integration/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:s}},onPageTransitionStart:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+onPageTransitionStart.ts",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:d}},onPageTransitionEnd:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+onPageTransitionEnd.ts",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:r}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/8/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:p}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"vike-react/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},Layout:{type:"cumulative",definedAtData:[{filePathToShowToUser:"/layouts/LayoutDefault.tsx",fileExportPathToShowToUser:[]}],valueSerialized:[{type:"pointer-import",value:l}]},title:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+config.ts",fileExportPathToShowToUser:["default","title"]},valueSerialized:{type:"js-serialized",value:"Vike Talk"}},onAfterRenderClient:{type:"cumulative",definedAtData:[{filePathToShowToUser:"/pages/+onAfterRenderClient.ts",fileExportPathToShowToUser:[]}],valueSerialized:[{type:"plus-file",exportValues:o}]},Loading:{type:"standard",definedAtData:{filePathToShowToUser:"vike-react/__internal/integration/Loading",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:a}}};export{T as configValuesSerialized};
