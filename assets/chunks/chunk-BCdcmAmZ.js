b8efa6722cb88418e19773514ab84ff15af680be
brillout <github.com@brillout.com>
2025-10-08 10:05:49 +0000

    Deploying to gh-pages from @ brillout/vike-talk@41a6b5d22bbe02ae0942e778bddf2241484b4e6a 🚀

---
+++ assets/chunks/chunk-BCdcmAmZ.js
@@ -0,0 +1,1 @@
+import{j as n}from"./chunk-H5eqeL3w.js";function s({style:t,...e}){return n.jsx("div",{style:{display:"flex",justifyContent:"center",alignItems:"center",...t},...e})}export{s as C};
---
+++ assets/chunks/chunk-CBYjmtms.js
@@ -0,0 +1,1 @@
+import{j as r}from"./chunk-H5eqeL3w.js";function s(){return r.jsx(r.Fragment,{children:r.jsx("hr",{style:{opacity:.3}})})}export{s as H};
