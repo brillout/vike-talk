ecf2286907f3b82260ea1e002e3e9479a4f41b6b
Romuald Brillout <git@brillout.com>
2025-11-16 18:41:13 +0100

    simplify footer style

--- layouts/LayoutDefault.tsx
+++ layouts/LayoutDefault.tsx
@@ -52,7 +52,7 @@ function Footer() {
         {footerContent.map(({ name, numberOfSlides }) => (
           <div key={name}>
             <div>{name}</div>
-            <div className="footer-bullets">
+            <div>
               {' '}
               {Array(numberOfSlides)
                 .fill(undefined)
