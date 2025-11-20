a3391d86ca2f5873c74c5f5015a8321ff1c344d9
Romuald Brillout <git@brillout.com>
2025-11-20 07:57:10 +0100

    wip

--- pages/26/LivingAt.tsx
+++ pages/26/LivingAt.tsx
@@ -1,3 +1,3 @@
 export function LivingAt({ children }: any) {
-  return <span style={{ color: '#aaa' }}>• {children}</span>
+  return <span style={{ color: '#aaa', marginLeft: 4, fontSize: '0.85em' }}>{children}</span>
 }
