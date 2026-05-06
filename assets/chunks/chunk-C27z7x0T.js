9aff6d23fbb3cee81ce4f8a1513df857aa442667
brillout <github.com@brillout.com>
2026-05-06 19:25:35 +0000

    Deploying to gh-pages from @ brillout/vike-talk@dfe10ff145c16a38b53f381f43c32309b0f50c76 🚀

--- assets/chunks/chunk-C27z7x0T.js
+++
@@ -1,1 +0,0 @@
-import{i as e,n as t,r as n,t as r}from"./chunk-DkS-YFHP.js";function i(e){return parseInt(e.slice(1),10)}e();async function a(e,i){n(e,`navigate`),r();let{keepScrollPosition:a,overwriteLastHistoryEntry:o,pageContext:s}=i??{};await t({scrollTarget:{preserveScroll:a??!1},urlOriginal:e,overwriteLastHistoryEntry:o,pageContextInitClient:s})}export{i as n,a as t};
---
+++ assets/entries/pages_15.DeJyHcI2.js
@@ -0,0 +1,23 @@
+import{n as e}from"../chunks/chunk-Bk03VUnZ.js";import{a as t,i as n,n as r,o as i,r as a,s as o,t as s}from"../chunks/chunk-Dz_par7l.js";var c=e({default:()=>d}),l=n();function u(e){let t={code:`code`,li:`li`,p:`p`,ul:`ul`,...e.components};return(0,l.jsxs)(t.ul,{children:[`
+`,(0,l.jsxs)(t.li,{children:[`
+`,(0,l.jsx)(t.p,{children:`Stability`}),`
+`,(0,l.jsxs)(t.ul,{children:[`
+`,(0,l.jsx)(t.li,{children:`Bugs quickly fixed (usually under 24h)`}),`
+`,(0,l.jsx)(t.li,{children:`Deployment skew protection`}),`
+`,(0,l.jsx)(t.li,{children:`Infinite loop guards`}),`
+`,(0,l.jsxs)(t.li,{children:[(0,l.jsx)(t.code,{children:`>=400`}),` helpful warnings & error messages`]}),`
+`,(0,l.jsx)(t.li,{children:`...`}),`
+`]}),`
+`]}),`
+`,(0,l.jsxs)(t.li,{children:[`
+`,(0,l.jsx)(t.p,{children:`Minimal blockers => Freedom`}),`
+`,(0,l.jsxs)(t.ul,{children:[`
+`,(0,l.jsx)(t.li,{children:`Client only data fetching`}),`
+`,(0,l.jsx)(t.li,{children:`Fault tolerant`}),`
+`,(0,l.jsx)(t.li,{children:(0,l.jsx)(t.code,{children:`globalContext.{pages,config,...}`})}),`
+`,(0,l.jsx)(t.li,{children:(0,l.jsx)(t.code,{children:`pageContext.{isHistoryNavigation,...}`})}),`
+`,(0,l.jsx)(t.li,{children:(0,l.jsx)(t.code,{children:`pageContext.dangerouslyUseInternals`})}),`
+`,(0,l.jsx)(t.li,{children:`...`}),`
+`]}),`
+`]}),`
+`]})}function d(e={}){let{wrapper:t}=e.components||{};return t?(0,l.jsx)(t,{...e,children:(0,l.jsx)(u,{...e})}):u(e)}var f={hasServerOnlyHook:{type:`computed`,definedAtData:null,valueSerialized:{type:`js-serialized`,value:!1}},isClientRuntimeLoaded:{type:`computed`,definedAtData:null,valueSerialized:{type:`js-serialized`,value:!0}},onBeforeRenderEnv:{type:`computed`,definedAtData:null,valueSerialized:{type:`js-serialized`,value:null}},dataEnv:{type:`computed`,definedAtData:null,valueSerialized:{type:`js-serialized`,value:null}},guardEnv:{type:`computed`,definedAtData:null,valueSerialized:{type:`js-serialized`,value:null}},onRenderClient:{type:`standard`,definedAtData:{filePathToShowToUser:`vike-react/__internal/integration/onRenderClient`,fileExportPathToShowToUser:[]},valueSerialized:{type:`pointer-import`,value:o}},onPageTransitionStart:{type:`standard`,definedAtData:{filePathToShowToUser:`/+onPageTransitionStart.ts`,fileExportPathToShowToUser:[]},valueSerialized:{type:`plus-file`,exportValues:i}},onPageTransitionEnd:{type:`standard`,definedAtData:{filePathToShowToUser:`/+onPageTransitionEnd.ts`,fileExportPathToShowToUser:[]},valueSerialized:{type:`plus-file`,exportValues:t}},Page:{type:`standard`,definedAtData:{filePathToShowToUser:`/pages/15/+Page.mdx`,fileExportPathToShowToUser:[]},valueSerialized:{type:`plus-file`,exportValues:c}},hydrationCanBeAborted:{type:`standard`,definedAtData:{filePathToShowToUser:`vike-react/config`,fileExportPathToShowToUser:[`default`,`hydrationCanBeAborted`]},valueSerialized:{type:`js-serialized`,value:!0}},Layout:{type:`cumulative`,definedAtData:[{filePathToShowToUser:`/+Layout.tsx`,fileExportPathToShowToUser:[]}],valueSerialized:[{type:`plus-file`,exportValues:a}]},title:{type:`standard`,definedAtData:{filePathToShowToUser:`/+config.ts`,fileExportPathToShowToUser:[`default`,`title`]},valueSerialized:{type:`js-serialized`,value:`Vike Talk`}},onAfterRenderClient:{type:`cumulative`,definedAtData:[{filePathToShowToUser:`/+onAfterRenderClient.ts`,fileExportPathToShowToUser:[]}],valueSerialized:[{type:`plus-file`,exportValues:r}]},Loading:{type:`standard`,definedAtData:{filePathToShowToUser:`vike-react/__internal/integration/Loading`,fileExportPathToShowToUser:[]},valueSerialized:{type:`pointer-import`,value:s}}};export{f as configValuesSerialized};
