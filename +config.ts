6307d33f13e0ae68ea54f3fc3b06f5b7a0ea5a10
Romuald Brillout <git@brillout.com>
2026-05-16 10:32:29 +0200

    wip

--- +config.ts
+++ +config.ts
@@ -5,7 +5,7 @@ import vikeReact from 'vike-react/config'
 import type { Config } from 'vike/types'
 
 const config = {
-  title: 'Vike Talk',
+  title: 'vike-talk',
   prerender: {
     noExtraDir: true,
   },
